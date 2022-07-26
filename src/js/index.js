156aa2f612c3535fc78244a0dc46bc3fa0c4c8ae
evertonleonel <everton_leonel@hotmail.com>
2022-07-25 23:49:19 -0300

    adicionado efeito de transicao na tela 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 const body = document.querySelector('body');
 
 body.addEventListener('dblclick', () => {
-  window.location.href = '/src/html/login.html'
-})
+  window.location.href = '../html/login.html'
+});
+
+window.onload = () => {
+  const transicao_el = document.querySelector('.transicao');
+ 
+  setInterval (()=>{
+    transicao_el.classList.remove('esta-ativa');
+  },500);
+  
+};
