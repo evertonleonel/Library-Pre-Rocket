84bc469fef452db9695fa32ce7038475d8e5f59c
evertonleonel <everton_leonel@hotmail.com>
2022-07-26 14:07:20 -0300

    adicionado tela 2, html,css ok, js basico

---
+++ src/js/login.js
@@ -0,0 +1,27 @@
+const form = document.forms.registro;
+const { email, senha } = form;
+
+window.onload = () => {
+  const transicao_el = document.querySelector('.transicao');
+ 
+  setInterval (()=>{
+    transicao_el.classList.remove('esta-ativa');
+  },500);
+  
+};
+
+form.addEventListener('submit', (e) => {
+  
+  if (!email.value || !senha.value){
+    e.preventDefault();
+  }
+
+  function logar(e){
+    e.preventDefault();
+    window.location.href = "/src/html/home.html"
+  }
+  
+  logar(e)
+
+});
+
