642c08ddf10ddadae1f99ebd879927d5ed011729
evertonleonel <everton_leonel@hotmail.com>
2022-07-26 22:53:37 -0300

    adicionada tela 3, html ok, css com js on-off; js apenas botao sair

---
+++ src/js/menu.js
@@ -0,0 +1,19 @@
+(function(){
+  const $body = document.querySelector('body');
+  $body.classList.remove('no-js');
+  $body.classList.add('js');
+  
+  const $logout = document.querySelector('.cabecalho_logout');
+  $logout.removeAttribute('style')
+
+  $logout.addEventListener('click', ()=> {
+    console.log($logout)
+    let $sair = document.querySelector('.cabecalho_sair ');
+    
+    let sairEstado = $sair.style.display;
+
+    $sair.style.display = sairEstado == 'none' ? 'block' : 'none';
+  })
+ 
+
+})()
