5d5be83598d28af1b8baea25aad4116fec6d16df
evertonleonel <everton_leonel@hotmail.com>
2022-08-01 15:14:51 -0300

    adicionado json de import e export + validacao login pelo json

--- src/global/js/global.js
+++ src/global/js/global.js
@@ -1,7 +1,16 @@
-let data;
+// let data;
 
-fetch('.data.jason').then(response => {
-  return response.json()
-}).then(body => {
-  data = body.data
-})
+// fetch('/data/data.json').then(response => {
+//   return response.json()
+// }).then(body => {
+//   data = body.data
+//   console.log(data.login[0].email, 'oi sou o login')
+// })
+
+const getData = async () => {
+  const response = await fetch('/data/data.json');
+  const data = await response.json();
+  return data;
+}
+
+export default getData;
--- src/paginas/login/login.js
+++ src/paginas/login/login.js
@@ -1,24 +1,51 @@
+import getData from "/src/global/js/global.js";
 const form = document.forms.registro;
 const { email, senha } = form;
 
-window.onload = () => {
-  const transicao_el = document.querySelector('.transicao');
+// window.onload = () => {
+//   const transicao_el = document.querySelector('.transicao');
  
-  setInterval (()=>{
-    transicao_el.classList.remove('esta-ativa');
-  },500);
-};
+//   setInterval (()=>{
+//     transicao_el.classList.remove('esta-ativa');
+//   },500);
+// };
 
-form.addEventListener('submit', (e) => {
-  
-  if (!email.value || !senha.value){
-    e.preventDefault();
-  }
+function logar(e){
+  e.preventDefault();
+  window.location.href = "/src/paginas/home/home.html"
+}
 
-  function logar(e){
-    e.preventDefault();
-    window.location.href = "/src/paginas/home/home.html"
-  }
+const verificarCampos = async () => {
+  const data = await getData();
   
-  logar(e)
-});
+  form.addEventListener('submit', (e) => {
+
+    if (!email.value || !senha.value){
+      e.preventDefault();
+      alert('Informe os campos')
+      return
+    }
+  
+    if(email.value === data.data.login[0].email && senha.value === data.data.login[0].password){
+      logar(e)
+    } else if (email.value === data.data.login[1].email && senha.value === data.data.login[1].password){
+      logar(e)
+    } else if (email.value === data.data.login[2].email && senha.value === data.data.login[2].password){
+      logar(e)
+    } else {
+      alert('Senha ou Usuário incorretos!')
+    }
+
+  });
+  
+}
+
+verificarCampos()
+
+
+
+
+
+
+
+  
