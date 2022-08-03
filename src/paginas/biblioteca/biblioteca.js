134ebce4e42182e81e506396530d44aa7a937f5a
evertonleonel <everton_leonel@hotmail.com>
2022-08-02 23:38:43 -0300

    codigo refatorado - agora faz tudo pelo JSON

--- src/paginas/biblioteca/biblioteca.js
+++ src/paginas/biblioteca/biblioteca.js
@@ -1,89 +1,51 @@
 import getData from "/src/global/js/global.js";
 
 const inputPesquisar = document.querySelector('.campo--pesquisar')
-const inputFiltro = document.querySelector('.campo--filtro')
+const selectFiltro = document.querySelector('.campo--filtro')
 const botaoBuscar = document.querySelector('.botao-buscar')
 const livrosContainer = document.querySelector('.container-livros')
 
-window.onload = async () => {
+const renderizarLivros = (livros) => {
+  livrosContainer.innerHTML = '';
 
-  const data =  await getData();
-  const {books} = data.data;
-  
-  books.forEach(item => {
-    const p = document.createElement('p');
-    let autor = document.createTextNode(item.author);
-    p.appendChild(autor)
-
-    const p1 = document.createElement('p');
-    let genero = document.createTextNode(item.genre);
-    p1.appendChild(genero)
-
-    const p2 = document.createElement('p');
-    let dataEntrada = document.createTextNode(item.systemEntryDate);
-    p2.appendChild(dataEntrada)
-
+  livros.forEach(item => {
+   
     const h3 = document.createElement('h3');
-    let titulo = document.createTextNode(item.tittle)
-    h3.appendChild(titulo)
+    let titulo = document.createTextNode(item.tittle);
+    h3.appendChild(titulo);
     h3.classList.add('livro-descricao_nome');
    
     const img = document.createElement('img');
-    img.src = item.image
+    img.src = item.image;
     img.classList.add('livro-descricao_nome', 'livro-descricao_imagem')
 
     const div = document.createElement('div');
     div.classList.add('livro-descricao');
-    div.appendChild(img)
-    div.appendChild(h3)
-    div.appendChild(p)
-    div.appendChild(p1)
-    div.appendChild(p2)
-  
-    livrosContainer.appendChild(div)
+    div.appendChild(img);
+    div.appendChild(h3);
+   
+    livrosContainer.appendChild(div);
   });
 }
 
-const esconderLivros = (divLivros, inputValue) => {
-  divLivros
-  .filter(livro =>  !livro.textContent.toLowerCase().includes(inputValue))
-  .forEach(livro => {
-   livro.classList.add('hidden')
-  })
+const filtrarLivros = async (pesquisa, tipo) => {
+  const data =  await getData();
+  const { books } = data.data;
+
+  const livrosFiltrados = books.filter((livro) => (
+    livro[tipo].toLowerCase().indexOf(pesquisa.toLowerCase()) > -1
+  ))
+
+  renderizarLivros(livrosFiltrados);
 }
 
-const mostrarLivros = (divLivros, inputValue) => {
-  divLivros
-  .filter(livro =>  livro.textContent.toLowerCase().includes(inputValue))
-  .forEach(livro => {
-   livro.classList.remove('hidden')
-  })
-}
-
-inputPesquisar.addEventListener('input', event => {
-  const inputValue = event.target.value.trim().toLowerCase();
-  const divLivros = Array.from(livrosContainer.children)
-
-  esconderLivros(divLivros, inputValue);
-  mostrarLivros(divLivros, inputValue);
+botaoBuscar.addEventListener('click', (e) => {
+  filtrarLivros(inputPesquisar.value, selectFiltro.value);
 })
-  
-// botaoBuscar.addEventListener('click', () => {
 
-  
-// })
+window.onload = async () => {
+  const data =  await getData();
+  const { books } = data.data;
 
-// function selecionou(){
-//   const select = inputFiltro.value
-
-//   if (select == '0'){
-//     console.log('olá')
-//   }else if (select == '1'){
-//     console.log('2')
-//   }else if (select == '2'){
-//     console.log('data')
-//   }
-// }
-
-// selecionou()
-
+  renderizarLivros(books);
+}
